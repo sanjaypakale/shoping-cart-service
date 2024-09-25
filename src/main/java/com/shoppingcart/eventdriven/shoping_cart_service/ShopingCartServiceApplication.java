4cb6ea7512ed89d58b6ea70ff1d75ad264a0fbb9
sanjay pakale <sanjaypakale@gmail.com>
2024-09-25 16:48:28 +0800

    initial commit

---
+++ src/main/java/com/shoppingcart/eventdriven/shoping_cart_service/ShopingCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eventdriven.shoping_cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingcart/eventdriven/shoping_cart_service/ShopingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.eventdriven.shoping_cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
